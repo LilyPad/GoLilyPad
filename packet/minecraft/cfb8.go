73d005dacfc315cf05a29d9d1ac654060a357163
Robert Coelho <me@iscoelho.com>
2013-12-04 14:34:36 -0800

    Cleanup

--- packet/minecraft/cfb8.go
+++ packet/minecraft/cfb8.go
@@ -4,9 +4,9 @@ import "crypto/cipher"
 import "errors"
 
 type cfb8 struct {
-	block			     cipher.Block
-	iv, initialIv, tmp   []byte
-	decrypt			     bool
+	block			cipher.Block
+	iv, initialIv, tmp	[]byte
+	decrypt			bool
 }
 
 func NewCFB8Decrypter(block cipher.Block, iv []byte) (stream cipher.Stream, err error) {
@@ -25,11 +25,11 @@ func newCFB8(block cipher.Block, iv []byte, decrypt bool) (stream cipher.Stream,
 	bytes := make([]byte, 256)
 	copy(bytes, iv)
 	return &cfb8{
-		block:	   block,
-		iv:		   bytes[:16],
-		initialIv: bytes,
-		tmp:	   make([]byte, 16),
-		decrypt:   decrypt,
+		block:		block,
+		iv:		bytes[:16],
+		initialIv:	bytes,
+		tmp:		make([]byte, 16),
+		decrypt:	decrypt,
 	}, nil
 }
 
@@ -39,15 +39,15 @@ func (this *cfb8) XORKeyStream(dst, src []byte) {
 		this.block.Encrypt(this.tmp, this.iv)
 		val = src[i] ^ this.tmp[0]
 		if cap(this.iv) >= 17 {
-				this.iv = this.iv[1:17]
+			this.iv = this.iv[1:17]
 		} else {
-				copy(this.initialIv, this.iv[1:])
-				this.iv = this.initialIv[:16]
+			copy(this.initialIv, this.iv[1:])
+			this.iv = this.initialIv[:16]
 		}
 		if this.decrypt {
-				this.iv[15] = src[i]
+			this.iv[15] = src[i]
 		} else {
-				this.iv[15] = val
+			this.iv[15] = val
 		}
 		dst[i] = val
 	}
