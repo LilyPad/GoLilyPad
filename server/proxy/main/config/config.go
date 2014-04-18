d8cade39820720bff067e50bec9bc3b530c2e8c5
Robert Coelho <me@iscoelho.com>
2014-04-18 06:01:34 -0700

    exmaple -> example ;o

--- server/proxy/main/config/config.go
+++ server/proxy/main/config/config.go
@@ -159,7 +159,7 @@ func DefaultConfig() (config *Config) {
 			Routes: []ConfigProxyRoute{
 				ConfigProxyRoute{"", "example", nil, "", nil, "", nil},
 				ConfigProxyRoute{"example.com", "", []string{"hub1", "hub2"}, "Example Custom MOTD", nil, "", nil},
-				ConfigProxyRoute{"hub.exmaple.com", "hub", nil, "", []string{"Example MOTD 1", "Example MOTD 2"}, "", nil},
+				ConfigProxyRoute{"hub.example.com", "hub", nil, "", []string{"Example MOTD 1", "Example MOTD 2"}, "", nil},
 				ConfigProxyRoute{"icon.exmaple.com", "hub", nil, "", nil, "icon.png", []string{"icon1.png", "icon2.png", "icons/icon3.png"}},
 			},
 			Locale: ConfigProxyLocale{
