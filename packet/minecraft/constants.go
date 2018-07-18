6131643f606f086db47c510461930a07a557929d
Robert Coelho <me@iscoelho.com>
2018-07-18 10:50:12 -0700

    1.13 protocol versions

--- packet/minecraft/constants.go
+++ packet/minecraft/constants.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	STRING_VERSION = string("1.12.2")
+	STRING_VERSION = string("1.13")
 	MAGIC          = string("§")
 
 	PACKET_SERVER_HANDSHAKE = 0x00
@@ -16,7 +16,7 @@ const (
 	PACKET_SERVER_STATUS_PING     = 0x01
 )
 
-var Versions = []int{340, 338, 335, 316, 315, 210, 110, 109, 108, 107, 47, 5, 4}
+var Versions = []int{393, 340, 338, 335, 316, 315, 210, 110, 109, 108, 107, 47, 5, 4}
 
 var HandshakePacketServerCodec = packet.NewPacketCodecRegistryDual([]packet.PacketCodec{}, []packet.PacketCodec{
 	PACKET_SERVER_HANDSHAKE: new(CodecServerHandshake),
