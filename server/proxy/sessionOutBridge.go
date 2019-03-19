2b12e79d3935e7c3cdca655be585176d1ad0bf64
Robert Coelho <me@iscoelho.com>
2019-03-19 01:55:15 -0400

    Prevent panics in redirects due to async map iteration race in PacketClientJoinGame

--- server/proxy/sessionOutBridge.go
+++ server/proxy/sessionOutBridge.go
@@ -141,7 +141,7 @@ func (this *SessionOutBridge) handlePacket(packet packet.Packet) (err error) {
 	switch this.state {
 	case STATE_LOGIN:
 		if packet.Id() == this.protocol.IdMap.PacketClientLoginSuccess {
-			this.session.redirectMutex.Lock()
+			this.session.redirectMutex.Lock() // Prevent other outbridges from connecting
 			this.state = STATE_INIT
 			this.session.redirecting = true
 			this.EnsureCompression()
@@ -168,133 +168,140 @@ func (this *SessionOutBridge) handlePacket(packet packet.Packet) (err error) {
 		fallthrough
 	case STATE_CONNECTED:
 		if this.state == STATE_CONNECTED {
-			this.session.redirectMutex.Lock()
-			if this.session.outBridge != this {
+			this.session.redirectMutex.Lock() // Ensure we are the current outbridge
+			if this.session.outBridge == this {
+				err = this.handlePacketConnected(packet)
+			} else {
+				// If not, close ourself
 				this.session.outBridge.EnsureCompression()
 				this.conn.Close()
-				this.session.redirectMutex.Unlock()
-				break
 			}
 			this.session.redirectMutex.Unlock()
-		}
-		id := packet.Id()
-		if id == this.protocol.IdMap.PacketClientJoinGame {
-			joinGamePacket := packet.(*minecraft.PacketClientJoinGame)
-			if this.session.mcBrand != nil {
-				this.Write(this.session.mcBrand)
-			}
-			if this.session.clientSettings != nil {
-				this.Write(this.session.clientSettings)
-			}
-			this.session.serverEntityId = joinGamePacket.EntityId
-			if this.session.state == STATE_INIT {
-				this.session.clientEntityId = joinGamePacket.EntityId
-			} else {
-				var swapDimension int32
-				if joinGamePacket.Dimension == 0 {
-					swapDimension = 1
-				} else {
-					swapDimension = 0
-				}
-				this.session.Write(minecraft.NewPacketClientRespawn(this.protocol.IdMap, swapDimension, 2, 0, "DEFAULT"))
-				this.session.Write(minecraft.NewPacketClientRespawn(this.protocol.IdMap, int32(joinGamePacket.Dimension), joinGamePacket.Difficulty, joinGamePacket.Gamemode, joinGamePacket.LevelType))
-				if len(this.session.playerList) > 0 {
-					if this.session.protocolVersion <= mc17.VersionNum {
-						for player, _ := range this.session.playerList {
-							this.session.Write(mc17.NewPacketClientPlayerListRemove(player))
-						}
-					} else {
-						items := make([]minecraft.PacketClientPlayerListItem, 0, len(this.session.playerList))
-						for uuidString, _ := range this.session.playerList {
-							uuid, _ := uuid.FromBytes([]byte(uuidString))
-							items = append(items, minecraft.PacketClientPlayerListItem{UUID: uuid})
-						}
-						this.session.Write(minecraft.NewPacketClientPlayerList(this.protocol.IdMap, minecraft.PACKET_CLIENT_PLAYER_LIST_ACTION_REMOVE, items))
-					}
-					this.session.playerList = make(map[string]struct{})
-				}
-				if len(this.session.scoreboards) > 0 {
-					for scoreboard, _ := range this.session.scoreboards {
-						this.session.Write(minecraft.NewPacketClientScoreboardObjectiveRemove(this.protocol.IdMap, scoreboard))
-					}
-					this.session.scoreboards = make(map[string]struct{})
-				}
-				if len(this.session.teams) > 0 {
-					for team, _ := range this.session.teams {
-						this.session.Write(minecraft.NewPacketClientTeamsRemove(this.protocol.IdMap, team))
-					}
-					this.session.teams = make(map[string]struct{})
-				}
-				if len(this.session.pluginChannels) > 0 {
-					channels := make([][]byte, 0, len(this.session.pluginChannels))
-					for channel, _ := range this.session.pluginChannels {
-						channels = append(channels, []byte(channel))
-					}
-					this.Write(minecraft.NewPacketServerPluginMessage(this.protocol.IdMap, "REGISTER", bytes.Join(channels, []byte{0})))
-				}
-				if len(this.session.bossBars) > 0 {
-					for uuidString, _ := range this.session.bossBars {
-						uuid, _ := uuid.FromBytes([]byte(uuidString))
-						this.session.Write(mc19.NewPacketClientBossBarRemove(uuid))
-					}
-				}
-				return
-			}
-		} else if id == this.protocol.IdMap.PacketClientPlayerList {
-			if this.session.protocolVersion <= mc17.VersionNum {
-				playerListPacket := packet.(*mc17.PacketClientPlayerList)
-				if playerListPacket.Online {
-					this.session.playerList[playerListPacket.Name] = struct{}{}
-				} else {
-					delete(this.session.playerList, playerListPacket.Name)
-				}
-			} else {
-				playerListPacket := packet.(*minecraft.PacketClientPlayerList)
-				if playerListPacket.Action == minecraft.PACKET_CLIENT_PLAYER_LIST_ACTION_ADD {
-					for _, item := range playerListPacket.Items {
-						this.session.playerList[string(item.UUID[:])] = struct{}{}
-					}
-				} else if playerListPacket.Action == minecraft.PACKET_CLIENT_PLAYER_LIST_ACTION_REMOVE {
-					for _, item := range playerListPacket.Items {
-						delete(this.session.playerList, string(item.UUID[:]))
-					}
-				}
-			}
-		} else if id == this.protocol.IdMap.PacketClientScoreboardObjective {
-			scoreboardPacket := packet.(*minecraft.PacketClientScoreboardObjective)
-			if scoreboardPacket.Action == minecraft.PACKET_CLIENT_SCOREBOARD_OBJECTIVE_ACTION_ADD {
-				this.session.scoreboards[scoreboardPacket.Name] = struct{}{}
-			} else if scoreboardPacket.Action == minecraft.PACKET_CLIENT_SCOREBOARD_OBJECTIVE_ACTION_REMOVE {
-				delete(this.session.scoreboards, scoreboardPacket.Name)
-			}
-		} else if id == this.protocol.IdMap.PacketClientTeams {
-			teamPacket := packet.(*minecraft.PacketClientTeams)
-			if teamPacket.Action == minecraft.PACKET_CLIENT_TEAMS_ACTION_ADD {
-				this.session.teams[teamPacket.Name] = struct{}{}
-			} else if teamPacket.Action == minecraft.PACKET_CLIENT_TEAMS_ACTION_REMOVE {
-				delete(this.session.teams, teamPacket.Name)
-			}
-		} else if id == this.protocol.IdMap.PacketClientBossBar {
-			bossBarPacket := packet.(*mc19.PacketClientBossBar)
-			if bossBarPacket.Action == mc19.PACKET_CLIENT_BOSS_BAR_ACTION_ADD {
-				this.session.bossBars[string(bossBarPacket.UUID[:])] = struct{}{}
-			} else if bossBarPacket.Action == mc19.PACKET_CLIENT_BOSS_BAR_ACTION_REMOVE {
-				delete(this.session.bossBars, string(bossBarPacket.UUID[:]))
-			}
-		} else if id == this.protocol.IdMap.PacketClientDisconnect {
-			this.state = STATE_DISCONNECTED
-			this.session.DisconnectJson(packet.(*minecraft.PacketClientDisconnect).Json)
-			return
-		} else if id == this.protocol.IdMap.PacketClientSetCompression {
-			this.SetCompression(packet.(*minecraft.PacketClientSetCompression).Threshold)
-			return
 		} else {
-			if genericPacket, ok := packet.(*minecraft.PacketGeneric); ok {
-				genericPacket.SwapEntities(this.session.clientEntityId, this.session.serverEntityId, true)
+			err = this.handlePacketConnected(packet)
+		}
+	}
+	return
+}
+
+func (this *SessionOutBridge) handlePacketConnected(packet packet.Packet) (err error) {
+	id := packet.Id()
+	if id == this.protocol.IdMap.PacketClientJoinGame {
+		joinGamePacket := packet.(*minecraft.PacketClientJoinGame)
+		if this.session.mcBrand != nil {
+			this.Write(this.session.mcBrand)
+		}
+		if this.session.clientSettings != nil {
+			this.Write(this.session.clientSettings)
+		}
+		this.session.serverEntityId = joinGamePacket.EntityId
+		if this.session.state == STATE_INIT {
+			this.session.clientEntityId = joinGamePacket.EntityId
+		} else {
+			var swapDimension int32
+			if joinGamePacket.Dimension == 0 {
+				swapDimension = 1
+			} else {
+				swapDimension = 0
+			}
+			this.session.Write(minecraft.NewPacketClientRespawn(this.protocol.IdMap, swapDimension, 2, 0, "DEFAULT"))
+			this.session.Write(minecraft.NewPacketClientRespawn(this.protocol.IdMap, int32(joinGamePacket.Dimension), joinGamePacket.Difficulty, joinGamePacket.Gamemode, joinGamePacket.LevelType))
+			if len(this.session.playerList) > 0 {
+				if this.session.protocolVersion <= mc17.VersionNum {
+					for player, _ := range this.session.playerList {
+						this.session.Write(mc17.NewPacketClientPlayerListRemove(player))
+					}
+				} else {
+					items := make([]minecraft.PacketClientPlayerListItem, 0, len(this.session.playerList))
+					for uuidString, _ := range this.session.playerList {
+						uuid, _ := uuid.FromBytes([]byte(uuidString))
+						items = append(items, minecraft.PacketClientPlayerListItem{UUID: uuid})
+					}
+					this.session.Write(minecraft.NewPacketClientPlayerList(this.protocol.IdMap, minecraft.PACKET_CLIENT_PLAYER_LIST_ACTION_REMOVE, items))
+				}
+				this.session.playerList = make(map[string]struct{})
+			}
+			if len(this.session.scoreboards) > 0 {
+				for scoreboard, _ := range this.session.scoreboards {
+					this.session.Write(minecraft.NewPacketClientScoreboardObjectiveRemove(this.protocol.IdMap, scoreboard))
+				}
+				this.session.scoreboards = make(map[string]struct{})
+			}
+			if len(this.session.teams) > 0 {
+				for team, _ := range this.session.teams {
+					this.session.Write(minecraft.NewPacketClientTeamsRemove(this.protocol.IdMap, team))
+				}
+				this.session.teams = make(map[string]struct{})
+			}
+			if len(this.session.pluginChannels) > 0 {
+				channels := make([][]byte, 0, len(this.session.pluginChannels))
+				for channel, _ := range this.session.pluginChannels {
+					channels = append(channels, []byte(channel))
+				}
+				this.Write(minecraft.NewPacketServerPluginMessage(this.protocol.IdMap, "REGISTER", bytes.Join(channels, []byte{0})))
+			}
+			if len(this.session.bossBars) > 0 {
+				for uuidString, _ := range this.session.bossBars {
+					uuid, _ := uuid.FromBytes([]byte(uuidString))
+					this.session.Write(mc19.NewPacketClientBossBarRemove(uuid))
+				}
+			}
+			return
+		}
+	} else if id == this.protocol.IdMap.PacketClientPlayerList {
+		if this.session.protocolVersion <= mc17.VersionNum {
+			playerListPacket := packet.(*mc17.PacketClientPlayerList)
+			if playerListPacket.Online {
+				this.session.playerList[playerListPacket.Name] = struct{}{}
+			} else {
+				delete(this.session.playerList, playerListPacket.Name)
+			}
+		} else {
+			playerListPacket := packet.(*minecraft.PacketClientPlayerList)
+			if playerListPacket.Action == minecraft.PACKET_CLIENT_PLAYER_LIST_ACTION_ADD {
+				for _, item := range playerListPacket.Items {
+					this.session.playerList[string(item.UUID[:])] = struct{}{}
+				}
+			} else if playerListPacket.Action == minecraft.PACKET_CLIENT_PLAYER_LIST_ACTION_REMOVE {
+				for _, item := range playerListPacket.Items {
+					delete(this.session.playerList, string(item.UUID[:]))
+				}
 			}
 		}
-		this.session.Write(packet)
+	} else if id == this.protocol.IdMap.PacketClientScoreboardObjective {
+		scoreboardPacket := packet.(*minecraft.PacketClientScoreboardObjective)
+		if scoreboardPacket.Action == minecraft.PACKET_CLIENT_SCOREBOARD_OBJECTIVE_ACTION_ADD {
+			this.session.scoreboards[scoreboardPacket.Name] = struct{}{}
+		} else if scoreboardPacket.Action == minecraft.PACKET_CLIENT_SCOREBOARD_OBJECTIVE_ACTION_REMOVE {
+			delete(this.session.scoreboards, scoreboardPacket.Name)
+		}
+	} else if id == this.protocol.IdMap.PacketClientTeams {
+		teamPacket := packet.(*minecraft.PacketClientTeams)
+		if teamPacket.Action == minecraft.PACKET_CLIENT_TEAMS_ACTION_ADD {
+			this.session.teams[teamPacket.Name] = struct{}{}
+		} else if teamPacket.Action == minecraft.PACKET_CLIENT_TEAMS_ACTION_REMOVE {
+			delete(this.session.teams, teamPacket.Name)
+		}
+	} else if id == this.protocol.IdMap.PacketClientBossBar {
+		bossBarPacket := packet.(*mc19.PacketClientBossBar)
+		if bossBarPacket.Action == mc19.PACKET_CLIENT_BOSS_BAR_ACTION_ADD {
+			this.session.bossBars[string(bossBarPacket.UUID[:])] = struct{}{}
+		} else if bossBarPacket.Action == mc19.PACKET_CLIENT_BOSS_BAR_ACTION_REMOVE {
+			delete(this.session.bossBars, string(bossBarPacket.UUID[:]))
+		}
+	} else if id == this.protocol.IdMap.PacketClientDisconnect {
+		this.state = STATE_DISCONNECTED
+		this.session.DisconnectJson(packet.(*minecraft.PacketClientDisconnect).Json)
+		return
+	} else if id == this.protocol.IdMap.PacketClientSetCompression {
+		this.SetCompression(packet.(*minecraft.PacketClientSetCompression).Threshold)
+		return
+	} else {
+		if genericPacket, ok := packet.(*minecraft.PacketGeneric); ok {
+			genericPacket.SwapEntities(this.session.clientEntityId, this.session.serverEntityId, true)
+		}
 	}
+	this.session.Write(packet)
 	return
 }
 
@@ -304,7 +311,7 @@ func (this *SessionOutBridge) ErrorCaught(err error) {
 	this.session.activeServersLock.Unlock()
 	if this.state == STATE_INIT {
 		this.session.redirecting = false
-		this.session.redirectMutex.Unlock()
+		this.session.redirectMutex.Unlock() // Unprevent other outbridges from connecting
 	}
 	this.disconnectErr = err
 	if this.state != STATE_DISCONNECTED && this.session.outBridge == this {
